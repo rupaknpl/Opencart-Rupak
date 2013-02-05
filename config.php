8f4932214ad4f32151ef5328aaf9f166f29d1bd2
Rupak Nepali <rupakopencart@gmail.com>
2013-02-05 14:06:27 +0545

    New opencart installed
    New opencart 1.5.5.1 installed

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart-rupak/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart-rupak/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\xampp\htdocs\Opencart-Rupak/catalog/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\Opencart-Rupak/system/');
+define('DIR_DATABASE', 'D:\xampp\htdocs\Opencart-Rupak/system/database/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\Opencart-Rupak/catalog/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\Opencart-Rupak/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\Opencart-Rupak/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\Opencart-Rupak/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\Opencart-Rupak/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\Opencart-Rupak/download/');
+define('DIR_LOGS', 'D:\xampp\htdocs\Opencart-Rupak/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencartrupak');
+define('DB_PREFIX', 'rup_');
+?>
